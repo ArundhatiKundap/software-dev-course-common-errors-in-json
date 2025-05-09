8e84612a862931fb4ce092e7158dfdb36d6a2b7d
Arundhati Vibhute <arundhatik0@gmail.com>
2025-05-09 14:41:20 -0500

    Corrected Json

--- project-task.js
+++ project-task.js
@@ -44,31 +44,35 @@ but it contains multiple syntax errors that prevent it from being parsed.
 // 🔍 Provided JSON File with Errors
 // ============================================
 
-const invalidBookingJSON = `
+
+// invalid invalidBookingJSON key should be in double quotes.and no need to declare with const
 {
+"invalidBookingJSON" :         
+                                       //single quote should not be there.Equal to sign should not be there instead : should be there
+{                                                          
   "hotelName": "Grand City Hotel",
-  "checkInDate": "2024-05-15"
+  "checkInDate": "2024-05-15" ,       // comma should be there after checkInDate. after each property comma should be there.
   "checkOutDate": "2024-05-20",
   "guests": [
     {
-      name: "Alice Johnson",
+      "name": "Alice Johnson",        // name key should be in double quotes.Key always should be in double quotes
       "age": 30,
       "email": "alice.johnson@example.com"
     },
     {
       "name": "Bob Smith",
-      "age": undefined,
+      "age": null,                       // there can be null value not undefined. undefined is not allowed in json.
       "email": "bob.smith@example"
     }
   ],
   "roomDetails": {
     "type": "Suite",
     "pricePerNight": 200,
-    "amenities": ["WiFi", "Breakfast", "Parking",]
+    "amenities": ["WiFi", "Breakfast", "Parking"]        // In array after amanities there should not be comma
   }
 }
-`;
-
+                                                         // ending should not have semicolin or end string quote
+ }
 
 // ============================================
 // ✅ Requirements
@@ -90,12 +94,18 @@ const invalidBookingJSON = `
 /*
 💬 Reflect and answer the following:
 
-1️⃣ What tools or techniques did you use to identify the errors?
+1️⃣ What tools or techniques did you use to identify the errors? 
+---> I have used https://jsonlint.com/ for validate the json
 
 2️⃣ How did you confirm that your corrected JSON file was valid?
+----> In tool when i have copy json file it shows valid json.
 
 3️⃣ Which errors were the most difficult to spot? Why?
+----> missing syntax is hard to find out
 
 4️⃣ What strategies can help you avoid these kinds of errors in the future?
+
    (e.g., syntax highlighting, linters, writing JSON by example)
+
+ -----> validate json by using json validator tool
 */
